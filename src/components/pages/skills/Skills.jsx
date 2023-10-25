faa4c463f4c87d0f0d381a288ac4c34712132ca2
Luis carlos <luisxhl7@gmail.com>
2023-10-24 22:48:21 -0500

    adding new images in view of skills

--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { images } from "../../../assets/image";
-import './Skills.scss'
 import { CardSkills } from '../../molecules/card-skills/CardSkills';
 import { RandomStars } from '../../molecules/randomStars/RandomStars';
+import './Skills.scss'
 
 const skillList = [
   {
