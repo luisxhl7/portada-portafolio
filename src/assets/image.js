2dfa5bf955e2e828056a8019a7c3829ebfddcb04
Luis carlos <luisxhl7@gmail.com>
2023-10-24 20:41:01 -0500

    adding image of test in svg

--- src/assets/image.js
+++ src/assets/image.js
@@ -1,5 +1,5 @@
 import git from './git.png'
-import github from './github.webp'
+import github from './github.svg'
 import javaScript from './javaScript.png'
 import typeScript from './typescript.png'
 import react from './react.svg'
